3c962fb945e97fea22e595c2dd71b0f7b370c71e
StuartBeacon <stuart@beacondata.com.au>
2019-05-21 22:02:58 +1000

    Refactor acceptance test for kafka-connect S3

--- README.md
+++ README.md
@@ -11,26 +11,54 @@ your [terraform plugin directory][third-party-plugins] (typically `~/.terraform.
 
 ## Example
 
-Configure the provider directly, or set the Env variable `KAFKA_CONNECT_URL`
+Configure the provider directly by setting the following Env Variables:
+*  `KAFKA_CONNECT_URL` 
+*  `CLIENT_CERT` 
+*  `CLIENT_KEY`
 ```hcl
 provider "kafka-connect" {
-  url = "http://localhost:8083"
+  url = "https://kafka-connect.dev.moneynp.xinja.com.au"
+
 }
 
-resource "kafka-connect_connector" "sqlite-sink" {
-  name = "test-sink"
-
-  config = {
-    "name"            = "test-sink"
-    "connector.class" = "io.confluent.connect.jdbc.JdbcSinkConnector"
-    "tasks.max"       = "1"
-    "topics"          = "orders"
-    "connection.url"  = "jdbc:sqlite:test.db"
-    "auto.create"     = "true"
-  }
+resource "kafka-connect_connector" "test" {
+	name = "acc-test"
+	
+	config = {
+	  "name"                   = "acc-test"
+	  "connector.class"        = "io.confluent.connect.s3.S3SinkConnector"
+	  "partition.duration.ms"  = "3600000"
+	  "s3.region"              = "ap-southeast-2"
+	  "flush.size"             = "10"
+	  "tasks.max"              = "1"
+	  "timezone"               = "UTC"
+	  "topics"                 = "platform-event.v1"
+	  "locale"                 = "US"
+	  "format.class"           = "io.confluent.connect.s3.format.avro.AvroFormat"
+	  "partitioner.class"      = "io.confluent.connect.storage.partitioner.TimeBasedPartitioner"
+	  "schema.generator.class" = "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator"
+	  "storage.class"          = "io.confluent.connect.s3.storage.S3Storage"
+	  "s3.bucket.name"         = "xinja-data-nonprod-platform-events"
+	  "path.format"            = "'year'=YYYY/'month'=MM/'day'=dd/'hour'=HH/"
+	  "timestamp.extractor"    = "Record"
+	}
 }
 ```
 
+## Testing
+
+Execute the following to run terraform provider acceptance tests.
+```sh
+# Sets the Kafka url
+export URL=https://localhost:8083
+
+# Params required to do mutual TLS
+export CERT=client.pem
+export KEY=client.key
+
+# Runs terraform acceptance testing
+make testacc URL=URL CERT=CERT KEY=KEY
+```
 [1]: https://www.terraform.io
 [2]: https://kafka.apache.org/documentation/#connect
 [third-party-plugins]: https://www.terraform.io/docs/configuration/providers.html#third-party-plugins
--- connect/resource_kafka_connector_test.go
+++ connect/resource_kafka_connector_test.go
@@ -6,7 +6,7 @@ import (
 
 	r "github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	kc "github.com/ricardo-ch/go-kafka-connect/lib/connectors"
+	kc "github.com/razbomi/go-kafka-connect/lib/connectors"
 )
 
 func TestAccConnectorConfigUpdate(t *testing.T) {
@@ -43,34 +43,34 @@ func testResourceConnector_initialCheck(s *terraform.State) error {
 		return fmt.Errorf("id doesn't match name")
 	}
 
-	client := testProvider.Meta().(kc.Client)
+	client := testProvider.Meta().(kc.HighLevelClient)
 
-	c, err := client.GetConnector(kc.ConnectorRequest{Name: "sqlite-sink"})
+	c, err := client.GetConnector(kc.ConnectorRequest{Name: "acc-test"})
 	if err != nil {
 		return err
 	}
 
-	tasksMax := c.Config["tasks.max"]
-	expected := "2"
-	if tasksMax != expected {
-		return fmt.Errorf("tasks.max should be %s, got %s connector not updated. \n %v", expected, tasksMax, c.Config)
+	flushSize := c.Config["flush.size"]
+	expected := "10"
+	if flushSize != expected {
+		return fmt.Errorf("flush.size should be %s, got %s connector not updated. \n %v", expected, flushSize, c.Config)
 	}
 
 	return nil
 }
 
 func testResourceConnector_updateCheck(s *terraform.State) error {
-	client := testProvider.Meta().(kc.Client)
+	client := testProvider.Meta().(kc.HighLevelClient)
 
-	c, err := client.GetConnector(kc.ConnectorRequest{Name: "sqlite-sink"})
+	c, err := client.GetConnector(kc.ConnectorRequest{Name: "acc-test"})
 	if err != nil {
 		return err
 	}
 
-	tasksMax := c.Config["tasks.max"]
-	expected := "1"
-	if tasksMax != expected {
-		return fmt.Errorf("tasks.max should be %s, got %s connector not updated. \n %v", expected, tasksMax, c.Config)
+	flushSize := c.Config["flush.size"]
+	expected := "3"
+	if flushSize != expected {
+		return fmt.Errorf("flush.size should be %s, got %s connector not updated. \n %v", expected, flushSize, c.Config)
 	}
 
 	return nil
@@ -78,30 +78,50 @@ func testResourceConnector_updateCheck(s *terraform.State) error {
 
 const testResourceConnector_initialConfig = `
 resource "kafka-connect_connector" "test" {
-  name = "sqlite-sink"
-
-  config = {
-		"name" = "sqlite-sink"
-    "connector.class" = "io.confluent.connect.jdbc.JdbcSinkConnector"
-    "tasks.max"       = "2"
-    "topics"          = "orders"
-    "connection.url"  = "jdbc:sqlite:test.db"
-    "auto.create"     = "true"
+	name = "acc-test"
+	
+	config = {
+	  "name"                   = "acc-test"
+	  "connector.class"        = "io.confluent.connect.s3.S3SinkConnector"
+	  "partition.duration.ms"  = "3600000"
+	  "s3.region"              = "ap-southeast-2"
+	  "flush.size"             = "10"
+	  "tasks.max"              = "1"
+	  "timezone"               = "UTC"
+	  "topics"                 = "mobile-platform.sap-transaction-events.transactions.v4"
+	  "locale"                 = "US"
+	  "format.class"           = "io.confluent.connect.s3.format.avro.AvroFormat"
+	  "partitioner.class"      = "io.confluent.connect.storage.partitioner.TimeBasedPartitioner"
+	  "schema.generator.class" = "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator"
+	  "storage.class"          = "io.confluent.connect.s3.storage.S3Storage"
+	  "s3.bucket.name"         = "xinja-data-nonprod-platform-events"
+	  "path.format"            = "'year'=YYYY/'month'=MM/'day'=dd/'hour'=HH/"
+	  "timestamp.extractor"    = "Record"
+	}
   }
-}
 `
 
 const testResourceConnector_updateConfig = `
 resource "kafka-connect_connector" "test" {
-  name = "sqlite-sink"
-
-  config = {
-		"name" = "sqlite-sink"
-    "connector.class" = "io.confluent.connect.jdbc.JdbcSinkConnector"
-    "tasks.max"       = "1"
-    "topics"          = "orders"
-    "connection.url"  = "jdbc:sqlite:test.db"
-    "auto.create"     = "true"
+	name = "acc-test"
+	
+	config = {
+	  "name"                   = "acc-test"
+	  "connector.class"        = "io.confluent.connect.s3.S3SinkConnector"
+	  "partition.duration.ms"  = "3600000"
+	  "s3.region"              = "ap-southeast-2"
+	  "flush.size"             = "3"
+	  "tasks.max"              = "1"
+	  "timezone"               = "UTC"
+	  "topics"                 = "mobile-platform.sap-transaction-events.transactions.v4"
+	  "locale"                 = "US"
+	  "format.class"           = "io.confluent.connect.s3.format.avro.AvroFormat"
+	  "partitioner.class"      = "io.confluent.connect.storage.partitioner.TimeBasedPartitioner"
+	  "schema.generator.class" = "io.confluent.connect.storage.hive.schema.DefaultSchemaGenerator"
+	  "storage.class"          = "io.confluent.connect.s3.storage.S3Storage"
+	  "s3.bucket.name"         = "xinja-data-nonprod-platform-events"
+	  "path.format"            = "'year'=YYYY/'month'=MM/'day'=dd/'hour'=HH/"
+	  "timestamp.extractor"    = "Record"
+	}
   }
-}
 `
